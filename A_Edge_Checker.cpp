cd98fce3b2c8f21d1f02f1dc177f0dd7ac596b78
ctxnn <taneja.chirag.ct@gmail.com>
2025-10-08 19:39:09 +0530

    part 1

---
+++ A_Edge_Checker.cpp
@@ -0,0 +1,142 @@
+// https://atcoder.jp/contests/abc240/tasks/abc240_a
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+// C++ Standard Library Headers
+#include <algorithm>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+// C++11 Headers
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+
+using namespace std;
+
+// Useful typedefs for competitive programming
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+// Commonly used defines
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define REPR(i,a,b) for (int i = a; i >= b; i--)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define present(c,x) ((c).find(x) != (c).end())
+
+// Constants
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fLL;
+const double EPS = 1e-9;
+const int MOD = 1e9 + 7;
+const int dx[] = {-1, 0, 1, 0};
+const int dy[] = {0, 1, 0, -1};
+
+void solve(int a,int b) {
+    // 1 2 3 4 5 6 7 8 9 10 1 
+    unordered_map<int, int> c;
+    for(int i=1; i<=10; i++){
+        if(i==10){
+            c[10]=1;
+        }
+        else{
+        c[i] = i+1;
+        }
+    }
+    if(c[a] == b || a == c[b]){
+        cout << "Yes";
+    }
+    else{
+        cout << "No";
+    }
+}
+
+int main() {
+    int a, b;        // declare variables first
+    cin >> a >> b;   // take input
+    solve(a, b);     // pass them to your function
+    return 0;        // good practice to end main with return 0
+}
+
+// editorial solution  ->
+/*
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+  int a, b;
+  cin >> a >> b;
+  
+  if(b-a == 1 || b-a == 9) cout << "Yes" << endl;
+  else cout << "No" << endl;
+  
+  return 0;
+}
+*/
---
+++ A_Find_Multiple.cpp
@@ -0,0 +1,141 @@
+// https://atcoder.jp/contests/abc220/tasks/abc220_a
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+// C++ Standard Library Headers
+#include <algorithm>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+// C++11 Headers
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+
+using namespace std;
+
+// Useful typedefs for competitive programming
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+// Commonly used defines
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define REPR(i,a,b) for (int i = a; i >= b; i--)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define present(c,x) ((c).find(x) != (c).end())
+
+// Constants
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fLL;
+const double EPS = 1e-9;
+const int MOD = 1e9 + 7;
+const int dx[] = {-1, 0, 1, 0};
+const int dy[] = {0, 1, 0, -1};
+
+void solve(int A,int B,int C) {
+    int ans;
+    for(int i=A; i<=B; i++){
+        if (i % C ==0){
+            ans = i;
+            cout << ans;
+            return;
+        }
+    }
+    cout << -1;
+    return;
+}
+
+int main() {
+    int A, B, C;
+    cin >> A >> B >> C;
+    solve(A,B,C);
+    return 0;
+}
+
+// editorial solution ->
+
+/*
+O(1) time complexity 
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+signed main(){
+    ll a,b,c;
+    cin>>a>>b>>c;
+    ll Y = b / c * c; // Y is a multiplt of C not exceeding B
+    if(a <= Y){
+        cout<<Y<<endl;
+    }else{
+        cout<<-1<<endl;
+    }
+    return 0;
+}
+
+*/
